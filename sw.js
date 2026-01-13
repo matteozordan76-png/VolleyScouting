9c7e16ad59f9dcf2bbbdd6ea1583345fc755710e
matteozordan76-png <matteo.zordan76@gmail.com>
2026-01-13 09:54:47 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,12 +1,21 @@
 
-const CACHE_NAME = 'scout-volley-v1';
+const CACHE_NAME = 'scout-volley-v2';
+const ASSETS = [
+  './',
+  './index.html',
+  'https://cdn.tailwindcss.com',
+  'https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.18.5/xlsx.full.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js'
+];
+
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(['./', './index.html']);
+      return cache.addAll(ASSETS);
     })
   );
 });
+
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
